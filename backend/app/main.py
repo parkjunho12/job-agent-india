f81cad7fe6ec8c3975ddc3ed1bc292f8b0dee2e3
Junho Park <ghdlwnsgh12@naver.com>
2026-01-12 21:31:21 +0000

    ㅁㅇㅇ

--- backend/app/main.py
+++ backend/app/main.py
@@ -53,13 +53,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://jobagent-career.com",
-        "https://www.jobagent-career.com",
-        "chrome-extension://gaheedodgcbgnfehnoknpdfodiihlncb",
-        "http://localhost:5173" 
-    ],
+    allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
