8b0ad4169c31e5d6d38d59ae78fd07ebe5a99dcd
Junho Park <ghdlwnsgh12@naver.com>
2026-01-17 12:38:13 +0000

    Change name

--- frontend/src/components/CVSetupWizards.jsx
+++ frontend/src/components/CVSetupWizards.jsx
@@ -17,6 +17,7 @@ function CVSetupWizard({ onComplete, onSkip }) {
     queryKey: ['experiences'],
     queryFn: () => experiencesApi.getAll()
   })
+  
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
